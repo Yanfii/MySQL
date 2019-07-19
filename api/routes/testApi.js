2d1935f75b4c62ad6f8337921e37038b8c245dc1
Patrick Zhang <patrickzhangyf@hotmail.com>
2019-07-19 00:25:18 -0400

    first commit

---
+++ api/routes/testApi.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'bojana',
+	database: 'cs348'
+})
+
+connection.connect(function(err) {
+	(err)? console.log(err): console.log(connection);
+})
+
+router.get('/', function(req, res, next) {
+    connection.query("INSERT INTO client VALUES (3, 'test', 'ZHANG', '11111')", function(err, data) {
+        (err)?res.send(err):res.json({clients: data})
+    })
+});
+
+module.exports = router;
