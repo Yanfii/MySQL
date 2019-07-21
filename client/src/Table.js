3b32a47044c345981a3da7c8f44297f42a974cda
Patrick Zhang <patrickzhangyf@hotmail.com>
2019-07-21 16:45:29 -0400

    added app bar

--- client/src/Table.js
+++ client/src/Table.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import './Table.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 650,
-  },
-}));
 
 function createData (id, name, num) {
   return { id, name, num };
@@ -25,31 +15,29 @@ function createData (id, name, num) {
 var rows = [];
 
 export default function SimpleTable(props) {
-  const classes = useStyles();
   var clients = props.clients
   clients.forEach( (client) =>
     rows.push(createData(client.user_id, client.first_name + " " + client.last_name, client.phone_number))
   )
   
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
+    <Paper className="root">
+      <Table className="table">
         <TableHead>
           <TableRow>
             <TableCell>User ID</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Phone</TableCell>
-
+            <TableCell>Name</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Events</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.num}</TableCell>
+              <TableCell component="th" scope="row">{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.num}</TableCell>
+              <TableCell>View events</TableCell>
             </TableRow>
           ))}
         </TableBody>
